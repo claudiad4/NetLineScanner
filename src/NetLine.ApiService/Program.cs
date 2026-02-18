8af2a75559c5e428d97ed54f0b75cb2c30ed6ad2
Claudia <claudiadurka@gmail.com>
2026-02-19 00:28:54 +0100

    Clean architecture - dodanie src i test + zmiana w porgram cs - komentarze, język angielski

---
+++ src/NetLine.ApiService/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using NetLine.Infrastructure.Data;      
+using NetLine.Infrastructure.Services;   
+using NetLine.Application.Interfaces;   
+using NetLine.ApiService.Hubs;           
+using NetLine.ApiService.Services;       
+using NetLine.ApiService.Endpoints;      
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.AddNpgsqlDbContext<AppDbContext>("deviceinfo");
+
+builder.Services.AddSingleton<ISNMPService, SnmpService>();
+
+builder.Services.AddHostedService<DeviceMonitorService>();
+
+builder.Services.AddSignalR();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.EnsureCreatedAsync();
+}
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapDefaultEndpoints();
+
+app.MapHub<DeviceHub>("/devicehub"); // SignalR
+
+app.MapGet("/", () => "NetLine API - Monitoring system is ready.");
+
+app.MapDeviceEndpoints();
+
+app.Run();
