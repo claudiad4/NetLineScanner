6edafc208e64f8b6dcf247e2440207f9b21fd72e
Claudia <claudiadurka@gmail.com>
2026-03-26 21:59:59 +0100

    Dodanie klasy ze słownikiem

---
+++ src/NetLine.Domain/Entities/OIDDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLine.Domain.Entities
+{
+    internal class OIDDictionary
+    {
+
+            // System Group (RFC 1213)
+            public const string SysDescr = ".1.3.6.1.2.1.1.1.0";
+            public const string SysName = ".1.3.6.1.2.1.1.5.0";
+            public const string SysLocation = ".1.3.6.1.2.1.1.6.0";
+            public const string SysContact = ".1.3.6.1.2.1.1.4.0";
+            public const string SysUpTime = ".1.3.6.1.2.1.1.3.0";
+
+        // Interfaces Group
+        public const string SysInterfacesCount = ".1.3.6.1.2.1.2.1.0";
+    }
+}
