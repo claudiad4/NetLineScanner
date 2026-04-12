1f54e88d03abfd3c256f7c7afa89a6abbeecff53
Claudia <claudiadurka@gmail.com>
2026-04-12 18:47:48 +0200

    dodanie api client + program cs

--- src/NetLine.Web/Program.cs
+++ src/NetLine.Web/Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddHttpClient<DeviceApiClient>(client =>
         ?? "http://localhost:5000");
 });
 
+builder.Services.AddHttpClient<OfficeApiClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["services:apiservice:http:0"]
+        ?? builder.Configuration["services:apiservice:https:0"]
+        ?? "http://localhost:5000");
+});
+
 builder.Services.AddSingleton<AlertNotificationService>();
 
 var app = builder.Build();
